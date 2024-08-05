b04e5e7676d363efe3e11d49c6e62c5e31f96a04
Andy <ajcraig222@gmail.com>
2024-08-05 10:11:05 +0100

    coding assembly update

--- obj/Debug/net8.0/dotnet_coding.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet_coding.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet_coding")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cf4ac111c647c386997a2e892739f17ce195ca7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4afbdd92d8bb16d82e8352ac5c34ab8bcb6984da")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet_coding")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet_coding")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
